75ac5f4233f0ed36b6e4de91ed95b890ca0f0386
Math_F_94 <141909636+MathF94@users.noreply.github.com>
2023-08-16 14:43:00 +0200

    display img

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
                     <div>
                         {{session('status')}}
                     </div>
-                @endif          
+                @endif
                     <div class="mt-16">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                     
